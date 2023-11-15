24c62de522d922bee29e834348f1162dc8a80cb5
Julio Eduardo Gustavo Alvarado Ramirez <128263541+julioalvarado202307764@users.noreply.github.com>
2023-11-15 11:12:53 -0600

    First Example array-sharp

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,17 @@
+﻿
+/*string[] fraudulentOrderIDs = new string[3];//crear un array sin inizializar 
+
+fraudulentOrderIDs [0] = "A123";
+fraudulentOrderIDs [1] = "B456";
+fraudulentOrderIDs [2] = "C789";*/
+
+string[] fraudulentOrderIDs = {"A123", "B456", "C789"};//inicializar elementos en un array
+///fraudulentOrderIDs [3] = "D000";
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+Console.WriteLine($"Reaging First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process");
